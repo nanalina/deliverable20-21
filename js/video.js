175b658c31d731e23a9922448c14c167e97d4d6f
nanalina <144241671+nanalina@users.noreply.github.com>
2023-11-06 15:27:15 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,19 @@
 var video;
 
+// Page load
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
+	// Initialize 
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.pause();
+	video.loop = false;
+	video.volume = document.querySelector("#slider").value / 100;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
 
