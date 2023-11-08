d6e7ee1d6744e3b7de03d30b1afacbfa438b9449
nanalina <144241671+nanalina@users.noreply.github.com>
2023-11-07 22:39:45 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -16,7 +16,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	document.querySelector("volume").innerHTML = video.volume * 100 + "%";
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
 // Pause Button 
